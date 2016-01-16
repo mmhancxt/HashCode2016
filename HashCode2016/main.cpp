65a5e66f39e3d1713ae71afa406592150e21f880
mmhancxt@gmail.com <mmhancxt@gmail.com>
2016-01-16 23:12:55 +0100

    Initial Commit

---
+++ HashCode2016/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashCode2016
+//
+//  Created by Liang LIU on 16/01/2016.
+//  Copyright (c) 2016 Liang LIU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
