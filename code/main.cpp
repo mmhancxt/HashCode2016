b18c5409fbba60fc2e4ca55bbe5898d36d02778c
mmhancxt@gmail.com <mmhancxt@gmail.com>
2016-02-09 22:10:40 +0100

    For test question

--- code/main.cpp
+++ code/main.cpp
@@ -5,30 +5,21 @@
 //  Created by Liang LIU on 16/01/2016.
 //  Copyright (c) 2016 Liang LIU. All rights reserved.
 //
-#include "Piano.h"
 #include <iostream>
-#include <vector>
-#include <string>
-#include <boost/algorithm/string.hpp>
+#include "PaintFacade.hpp"
 
-void test_boost()
-{
-    std::string s = "a,b,c,d,e";
-    std::vector<std::string> fields;
-    boost::split(fields, s, boost::is_any_of(","));
-    std::copy(fields.begin(), fields.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-}
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "We are the champion!\n";
-    Piano p("Yamaha");
-    p.PrintMark();
-    auto c = 11;
-    std::cout << "c++" << c << std::endl;
-
-
-    test_boost();
-
+    FacadePrinter s;
+//    s.PrintWithHorizon();
+    // logo : 68
+    // right_angle: 124
+    // learn_and_teach : 3225
+    
+//    s.ReadData("logo.in");      // 64
+//    s.ReadData("right_angle.in"); // 92
+    s.ReadData("learn_and_teach.in");   // 3486
+    s.PrintImage();
     return 0;
 }
