229f47d6f451d936bab4b6acfcdf9163e2a3fab6
mmhancxt@gmail.com <mmhancxt@gmail.com>
2016-02-11 21:58:07 +0100

    Finish DeliveryByProduct

--- code/DeliveryByProduct.h
+++ code/DeliveryByProduct.h
@@ -15,6 +15,76 @@ class DeliveryByProduct
 public:
     DeliverByProduct();
     
+    void Delivery(InputLoader& loader, const Position& p, Drone& drone)
+    {
+        Order* nearestOrder = GetNearestOrder(loader, p, drone);
+        if (nearestOrder != nullptr)
+        {
+            for (auto it=drone.goods.begin(); drone.goods.end(); it++)
+            {
+                int productId = it->first;
+                int productCountInDrone = it->second;
+                auto iterator = nearestOrder->purchasedProducts.find(poroductId);
+                if (iterator != nearestOrder->purchasedProducts.end())
+                {
+                    int productCountInOrder = iterator->second;
+                    int deliverCount = std::min(productCountInDrone, productCountInOrder);
+                    deliver(drone.id, nearestOrder->id, productId, deliverCount);
+                    drone.load = drone.load - loader.products[productId]*deliverCount;
+                    if (productCountInDrone == deliverCount)
+                    {
+                        drone.goods.erase(productId);
+                    }
+                    else
+                    {
+                        drone.goods[productId] = productCountInDrone - deliverCount;
+                    }
+                    if (productCountInOrder == deliverCount)
+                    {
+                        nearestOrder->purchasedProducts.erase(productId);
+                    }
+                    else
+                    {
+                        nearestOrder->purchasedProducts[productId] = productCountInOrder - deliverCount;
+                    }
+                }
+            }
+            
+            if (load == 0)
+            {
+                Load(loader, p, drone);
+            }
+            else
+            {
+                Delivery(loader, p, drone);
+            }
+        }
+    }
+    
+    void Load(InputLoader& loader, const Position& p, Drone& drone)
+    {
+        // next status is load
+        int product = -1;
+        for (int productId = 0; productId < productNum.size() && product == -1; productId++)
+        {
+            if (productNum[productId] != 0)
+            {
+                product = productId;
+            }
+        }
+        int productSize = std::min((loader.const_maxDroneLoad-load)/loader.products[productId].weight, productNum[productId]);
+        
+        WareHouse* nearestHouse = GetNearestWareHouse(loader, p);
+        load(drone.id, nearestHouse->id, product, productSize);
+        productNum[productId]-= productSize;
+        //drone.nextAction = INWAREHOUSE;
+        drone.productId = product;
+        drone.productCount = productSize;
+        drone.load = productSize*loader.products[poroductId].weight;
+        drone.nextUsableTurn = CalculateEula(nearestHouse->position, p);
+  
+    }
+    
     void Start()
     {
         InputLoader loader;
@@ -38,49 +108,23 @@ public:
         {
             for (int i = 0 ;i < const_droneNum ; i++)
             {
+                Drone& drone = drowns[i];
                 if (drone.nextUsableTurn == turn)
-                {
-                    
-                }
-                if (drone.staus != FLY)
                 {
                     Position p = drone.position;
                     if (drone.load != 0)
                     {
-                        Order* nearestOrder = GetNearestOrder(loader, p, drone);
-                        if (nearestOrder != nullptr)
-                        {
-                            // Del
-                            deliver(drone.id, nearestOrder->id, int product_type, int count)
-
-                        }
+                        Delivery(loader, p, drone);
                     }
                     else
                     {
-                        // Load
-                        int product = -1;
-                        for (int productId = 0; productId < productNum.size() && product == -1; productId++)
-                        {
-                            if (productNum[productId] != 0)
-                            {
-                                product = productId;
-                            }
-                        }
-                        int productSize = std::min((loader.const_maxDroneLoad-load)/loader.products[productId].weight, productNum[productId]);
-                        
-                        WareHouse* nearestHouse = GetNearestWareHouse(loader, p);
-                        load(drone.id, nearestHouse->id, product, productSize);
-                        productNum[productId]-= productSize;
-                        drone.status = FLY;
-                        drone.productId = product;
-                        drone.productCount = productSize;
+                        Load(loader, p, drone);
                     }
-                    
                 }
             }
         }
-    
+        
     }
 };
-                
+
 #endif /* DeliveryByProduct_h */
